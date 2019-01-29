7bf99776da1aaa1e24b9b97e32976637fd4b2f68
Matthew Markwell <markwell@google.com>
2019-01-29 13:56:38 -0800

    Update kernel headers
    Test: atest :airbrush-platform-presubmit
    Change-Id: I7a46f0480ff778a929a5d578ffbd34c3451d90a3
    Signed-off-by: Matthew Markwell <markwell@google.com>

--- sm8150/kernel-headers/linux/faceauth.h
+++ sm8150/kernel-headers/linux/faceauth.h
@@ -20,6 +20,7 @@
 #define _LINUX_FACEAUTH_H
 #include <linux/types.h>
 #include <linux/ioctl.h>
+#include <linux/time.h>
 #define FACEAUTH_OP_ERASE 0
 #define FACEAUTH_OP_ENROLL 1
 #define FACEAUTH_OP_VALIDATE 2
@@ -27,6 +28,8 @@
 #define FACEAUTH_RESULT_SUCCESS 0
 #define FACEAUTH_RESULT_FAILURE 1
 #define FACEAUTH_ERROR_NO_ERROR 0
+#define FACEAUTH_MAX_TASKS 32
+#define FACEAUTH_DEBUG_REGISTER_COUNT (24)
 struct faceauth_start_data {
   __u8 profile_id;
   __u8 operation;
@@ -43,13 +46,56 @@ struct faceauth_start_data {
   __s32 error_code;
   __u32 fw_version;
 } __attribute__((packed));
+#define GET_DEBUG_DATA_FROM_CACHE (0)
+#define GET_DEBUG_DATA_FROM_AB_DRAM (1)
 struct faceauth_debug_data {
-  __u8 * print_buffer;
-  __u32 print_buffer_size;
+  union {
+    __u8 * debug_buffer;
+    __u8 * print_buffer;
+  };
+  union {
+    __u32 debug_buffer_size;
+    __u32 print_buffer_size;
+  };
+  __u32 flags;
+} __attribute__((packed));
+struct faceauth_workload_control {
+  __u32 workload_state;
+  __u32 run_count;
+  __u32 run_time_ms;
+  __u32 run_time_us;
+  __u32 status;
+} __attribute__((packed));
+struct faceauth_debug_register {
+  __u64 address;
+  __u64 value;
+} __attribute__((packed));
+struct faceauth_airbrush_state {
+  __u32 faceauth_version;
+  __s32 error_code;
+  __u32 internal_state_size;
+  __u32 command;
+  __u32 num_tasks;
+  __u32 register_list_length;
+  __u32 saved_register_count;
+  struct faceauth_workload_control control_list[FACEAUTH_MAX_TASKS];
+  struct faceauth_debug_register debug_registers[FACEAUTH_DEBUG_REGISTER_COUNT];
+} __attribute__((packed));
+struct faceauth_debug_image {
+  __u32 offset_to_image;
+  __u32 image_size;
+};
+struct faceauth_debug_entry {
+  struct timeval timestamp;
+  struct faceauth_debug_image left_dot;
+  struct faceauth_debug_image right_dot;
+  struct faceauth_debug_image flood;
+  struct faceauth_airbrush_state ab_state;
 } __attribute__((packed));
 #define FACEAUTH_DEV_IOC_INIT _IO('f', 1)
 #define FACEAUTH_DEV_IOC_START _IOWR('f', 2, struct faceauth_start_data)
 #define FACEAUTH_DEV_IOC_CLEANUP _IO('f', 4)
 #define FACEAUTH_DEV_IOC_DEBUG _IOR('f', 5, struct faceauth_debug_data)
+#define FACEAUTH_DEV_IOC_DEBUG_DATA _IOR('f', 6, struct faceauth_debug_data)
 #endif
 
