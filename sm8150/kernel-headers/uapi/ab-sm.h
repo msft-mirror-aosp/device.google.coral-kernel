26c9ec49888195aeaac3c44e42574c2914b50b4d
Shaun Kelsey <shaunkelsey@google.com>
2019-03-01 14:11:50 -0800

    Update AB uapi headers
    Bug: 126264567
    Test: http://sponge/312b614f-7652-4a95-b4f5-8b7d8c0a0bd3
    Change-Id: I460871e070938711bbd122c55c933165b7e8ee7f
    Signed-off-by: Shaun Kelsey <shaunkelsey@google.com>

--- sm8150/kernel-headers/uapi/ab-sm.h
+++ sm8150/kernel-headers/uapi/ab-sm.h
@@ -34,8 +34,63 @@
 #define AB_SM_SET_TPU_STATE _IOW(AB_SM_IOCTL_MAGIC, 11, int)
 #define AB_SM_SET_DDR_STATE _IOW(AB_SM_IOCTL_MAGIC, 12, int)
 #define AB_SM_SET_PCIE_STATE _IOW(AB_SM_IOCTL_MAGIC, 13, int)
+#define AB_SM_UPDATE_IPU_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 14, struct new_block_props *)
+#define AB_SM_UPDATE_TPU_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 15, struct new_block_props *)
+#define AB_SM_UPDATE_DRAM_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 16, struct new_block_props *)
+#define AB_SM_UPDATE_MIF_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 17, struct new_block_props *)
+#define AB_SM_UPDATE_FSYS_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 18, struct new_block_props *)
+#define AB_SM_UPDATE_AON_STATE_PROPERTIES _IOW(AB_SM_IOCTL_MAGIC, 19, struct new_block_props *)
+#define AB_SM_GET_EL2_MODE _IOR(AB_SM_IOCTL_MAGIC, 20, int *)
 #define AB_CHIP_ID_UNKNOWN - 1
 #define AB_CHIP_ID_A0 0
 #define AB_CHIP_ID_B0 1
+#define UAPI_BLK_(num,pmu,rail,v,clk,freq,pwr,used,tiles,dr) { UAPI_BLOCK_STATE_ ##num, (enum uapi_pmu_state) pmu, uapi_ ##rail, UAPI_VOLTAGE_ ##v, uapi_ ##clk, (__u64) (1000000. * freq), pwr, used, tiles, dr, }
+enum uapi_block_state {
+  UAPI_BLOCK_STATE_0_0 = 0,
+  UAPI_BLOCK_STATE_0_1,
+  UAPI_BLOCK_STATE_0_2,
+  UAPI_BLOCK_STATE_0_3,
+  UAPI_BLOCK_STATE_0_4,
+  UAPI_BLOCK_STATE_0_5,
+  UAPI_BLOCK_STATE_0_6,
+  UAPI_BLOCK_STATE_1_0 = 10,
+  UAPI_BLOCK_STATE_1_1,
+  UAPI_BLOCK_STATE_1_2,
+  UAPI_BLOCK_STATE_2_0 = 20,
+  UAPI_BLOCK_STATE_2_1,
+  UAPI_BLOCK_STATE_3_0 = 30,
+  UAPI_NUM_BLOCK_STATES,
+};
+enum uapi_pmu_state {
+  UAPI_PMU_STATE_ON = 0,
+  UAPI_PMU_STATE_SLEEP,
+  UAPI_PMU_STATE_DEEP_SLEEP,
+  UAPI_PMU_STATE_OFF
+};
+enum uapi_state {
+  uapi_off = 0,
+  uapi_on = 1,
+};
+enum uapi_logic_voltage {
+  UAPI_VOLTAGE_0_0,
+  UAPI_VOLTAGE_0_60,
+  UAPI_VOLTAGE_0_75,
+  UAPI_VOLTAGE_0_85,
+};
+struct uapi_block_properties {
+  enum uapi_block_state id;
+  enum uapi_pmu_state pmu;
+  enum uapi_state voltage_rail_status;
+  enum uapi_logic_voltage logic_voltage;
+  enum uapi_state clk_status;
+  __u64 clk_frequency;
+  __u32 num_powered_cores;
+  __u32 num_computing_cores;
+  __u32 num_powered_tiles;
+  __u32 data_rate;
+};
+struct new_block_props {
+  struct uapi_block_properties table[UAPI_NUM_BLOCK_STATES];
+};
 #endif
 
